c5b05b635b1a4a0b400d9868d6942895d273d123
Danya4kas <daniilmosiychuk007@gmail.com>
2024-12-04 22:25:01 +0200

    Delete lab_11 directory

--- lab_11/planet_script.js
+++
@@ -1,8 +0,0 @@
-    let urlParams = new URLSearchParams(window.location.search);
-    let planetKey = urlParams.get('planet');
-
-    if (planetKey) {
-        showPlanetDetails(planetKey);
-    } else {
-        document.getElementById("planetContent").innerHTML = "<p>Planet not found!</p>";
-    }
